e5b3a0bbe49ffb20835f773cee3e1561752d76be
dnlnmhl <mdanilin@fastmail.com>
2021-09-28 13:30:27 +0300

    one

---
+++ Test/js/script.js
@@ -0,0 +1,7 @@
+import React from "react";
+import {render} from "react-dom";
+
+const element = React.createElement("h1", {
+  className: "zag"
+}, "Yo!");
+const root = document.querySelector("#root");
